e9dae9b3307e460a9c12634756dd4d0c1d71b7ae
NFRiacowboy <me@nfriacowboy.com>
2021-05-13 16:01:14 +0100

    add more package and change header

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;;
 let appInsights = require('applicationinsights');
 
 appInsights.setup("e506aa3a-10af-4f22-a594-656fda5a89ce")
+.setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)  
 .setSendLiveMetrics(true);
 appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = "App2";
 appInsights.start();
